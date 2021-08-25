d53d2b27ad069e89feef055741620665654a6d14
Raaya <rahulagre555@gmail.com>
2021-08-25 19:59:46 +0530

    created dummy links

--- google-ui-clone/src/App.js
+++ google-ui-clone/src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import './App.css';
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Google Search Engine
-      </h1>
+        <Router>
 
-      {/*Home (the one with the search component)*/}
-      <Home />
-
+          <Switch>
+            <Route path="/search">
+            <h1>Google Search Engine</h1>
+            </Route>
+            <Route path="/">
+              {/*Home (the one with the search component)*/}
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
       {/* Search results page */}
-
     </div>
   );
 }
